bcaeba59df29ee2d53ca3a20cc8d22197bac0fc8
raghda-ali <raghdaali991@gmail.com>
2020-12-26 22:48:35 +0200

    seats

--- lib/Screens/addMovie.dart
+++ lib/Screens/addMovie.dart
@@ -19,7 +19,9 @@ class _AddMovieState extends State<AddMovie> {
   File _image;   
   
     String _title,_description,_photo,_time;
-    int _numberofseats;
+    bool _c1= false ,_c2= false ,_c3= false, _c4= false, _c5= false, _c6=false, _c7=false, _c8=false, _c9=false, _c10=false, _c11=false, _c12=false, _c13=false, _c14=false, _c15=false, _c16=false, _c17=false, _c18 =false, _c19=false  , _c20=false, 
+    _c21=false, _c22=false, _c23=false, _c24=false, _c25=false, _c26=false, _c27=false, _c28=false, _c29=false, _c30=false, _c31=false, _c32=false, _c33=false, _c34=false, _c35=false, _c36=false, _c37=false, _c38=false, _c39=false, _c40=false,
+    _c41=false , _c42=false, _c43=false, _c44=false, _c45=false, _c46=false, _c47=false ;
   Future getImageFromCam() async{
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
     setState(() {
@@ -36,9 +38,12 @@ class _AddMovieState extends State<AddMovie> {
   }
 
 Future uploadImage() async {    
-  Reference storageReference = FirebaseStorage.instance.ref().child('Uploads/image4');        
+  Reference storageReference = FirebaseStorage.instance.ref().child('Images/image1');        
   UploadTask uploadTask = storageReference.putFile(_image);   
    var  imageUrl= await (await uploadTask).ref.getDownloadURL();
+   print(imageUrl);
+   _photo = imageUrl;
+   print(_photo);
  }  
 @override
 Widget build(BuildContext context){
@@ -124,27 +129,7 @@ Widget build(BuildContext context){
        Padding(
           padding: EdgeInsets.only(top: 10.0),
          ),
-         TextFormField(
-        style: TextStyle(color : Colors.black),
-                  decoration: InputDecoration(
-                    labelText:"ImageUrl",
-                    border:OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(color:Colors.blue,width: 1),
-                      ), 
-                      disabledBorder: OutlineInputBorder(
-                        borderRadius:BorderRadius.circular(15),
-                        borderSide: BorderSide(color: Colors.blue)
-                         ),
-                  ),
-                  onSaved: (value)
-                  {
-                   _photo=value;
-                  },
-      ),
-       Padding(
-          padding: EdgeInsets.only(top: 10.0),
-         ),
+        
        TextFormField(
         style: TextStyle(color : Colors.black),
           controller: timeCtl,  // add this line.
@@ -168,27 +153,7 @@ Widget build(BuildContext context){
        Padding(
           padding: EdgeInsets.only(top: 10.0),
          ),
-       TextFormField(
-        style: TextStyle(color : Colors.black),
-                  decoration: InputDecoration(
-                    labelText:"Number of seats",
-                    border:OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(color:Colors.blue,width: 1),
-                      ), 
-                      disabledBorder: OutlineInputBorder(
-                        borderRadius:BorderRadius.circular(15),
-                        borderSide: BorderSide(color: Colors.blue)
-                         ),
-                  ),
-                  onSaved: (value)
-                  {
-                    _numberofseats = int.parse(value) ;
-                  },
-      ),
- Padding(
-          padding: EdgeInsets.only(top: 10.0),
-         ),
+       
              Row(
               mainAxisAlignment: MainAxisAlignment.center,
                children: [
@@ -205,7 +170,53 @@ Widget build(BuildContext context){
                           mDescription : _description,
                           mImage : _photo,
                           mTime : _time,
-                          mNumberofseats : _numberofseats
+                          c1: _c1 ,
+                          c2: _c2,
+                          c3: _c3,
+                          c4 : _c4,
+                          c5 : _c5,
+                          c6 : _c6,
+                          c7 : _c7 ,
+                          c8 : _c8,
+                          c9 : _c9,
+                          c10 : _c10,
+                          c11 : _c11,
+                          c12 : _c12,
+                          c13 : _c13,
+                          c14 : _c14,
+                          c15 : _c15,
+                          c16 : _c16,
+                          c17 : _c17,
+                          c18 : _c18,
+                          c19:  _c19,
+                          c20:  _c20,
+                          c21:  _c21,
+                          c22 : _c22,
+                          c23 : _c23,
+                          c24 : _c24,
+                          c25 : _c25,
+                          c26 : _c26,
+                          c27 : _c27,
+                          c28 : _c28,
+                          c29 : _c29,
+                          c30 : _c30,
+                          c31 : _c31,
+                          c32 : _c32,
+                          c33 : _c33,
+                          c34 : _c34,
+                          c35 : _c35,
+                          c36 : _c36,
+                          c37:  _c37,
+                          c38 : _c38,
+                          c39 : _c39,
+                          c40 : _c40,
+                          c41 : _c41,
+                          c42 : _c42,
+                          c43 : _c43,
+                          c44 : _c44,
+                          c45 : _c45,
+                          c46 : _c46,
+                          c47 : _c47,
                         ));
                       }
                     },  
--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -4,12 +4,105 @@ class Movie
   String mDescription;
   String mImage;
   String mTime;
-  int mNumberofseats;
-
+  bool c1 =false;
+  bool c2 =false;
+  bool c3 =false;
+  bool c4 =false;
+  bool c5 =false;
+  bool c6 =false;
+  bool c7 =false;
+  bool c8 =false;
+  bool c9 =false;
+  bool c10 =false;
+  bool c11 =false;
+  bool c12 =false;
+  bool c13 =false;
+  bool c14 =false;
+  bool c15 =false;
+  bool c16 =false;
+  bool c17 =false;
+  bool c18 =false;
+  bool c19 =false;
+  bool c20 =false;
+  bool c21 =false;
+  bool c22 =false;
+  bool c23 =false;
+  bool c24 =false;
+  bool c25 =false;
+  bool c26 =false;
+  bool c27 =false;
+  bool c28 =false;
+  bool c29 =false;
+  bool c30 =false;
+  bool c31 =false;
+  bool c32 =false;
+  bool c33 =false;
+  bool c34 =false;
+  bool c35 =false;
+  bool c36 =false;
+  bool c37 =false;
+  bool c38 =false;
+  bool c39 =false;
+  bool c40 =false;
+  bool c41 =false;
+  bool c42 =false;
+  bool c43 =false;
+  bool c44 =false;
+  bool c45 =false;
+  bool c46 =false;
+  bool c47 =false;
+  
   Movie(
     {this.mTitle,
-    this.mDescription,
+     this.mDescription,
      this.mImage ,
       this.mTime ,
-       this.mNumberofseats});
+      this.c1,
+      this.c2,
+      this.c3,
+      this.c4,
+      this.c5,
+      this.c6,
+      this.c7,
+      this.c8,
+      this.c9,
+      this.c10,
+      this.c11,
+      this.c12,
+      this.c13,
+      this.c14,
+      this.c15,
+      this.c16,
+      this.c17,
+      this.c18,
+      this.c19,
+      this.c20,
+      this.c21,
+      this.c22,
+      this.c23,
+      this.c24,
+      this.c25,
+      this.c26,
+      this.c27,
+      this.c28,
+      this.c29,
+      this.c30,
+      this.c31,
+      this.c32,
+      this.c33,
+      this.c34,
+      this.c35,
+      this.c36,
+      this.c37,
+      this.c38,
+      this.c39,
+      this.c40,
+      this.c41,
+      this.c42,
+      this.c43,
+      this.c44,
+      this.c45,
+      this.c46,
+      this.c47,
+      });
 }
